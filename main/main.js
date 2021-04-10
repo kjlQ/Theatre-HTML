d248ad4053ac54f85231f6a8e1bc0aa4f1b2fbec
kjlQ <rett8716@gmail.com>
2021-04-10 16:54:28 +0300

    css fix

--- main/main.js
+++ main/main.js
@@ -1,10 +1,8 @@
 $(".burg").on('click',function() {
-    $(".links").toggleClass("active")
     $(".links").toggleClass("disable")
     $(".all-links").toggleClass("active-sidebar")
 })
 $(".links li a").on("click",function() {
-    $(".links").toggleClass("active")
     $(".links").toggleClass("disable")
     $(".all-links").toggleClass("active-sidebar")
 })
